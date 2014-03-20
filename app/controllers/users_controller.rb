90f8915d26d729b026a4daa269182ba26ed70c3f
thuynt_57 <thuynt_57@vnu.edu.vn>
2014-03-20 22:18:44 +0700

    Page after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
     if @user.save
-      flash[:notice] = "You have signed up successfully."
-      #redirect_to users_path
+      #flash[:notice] = "You have signed up successfully."
+      #redirect_to root_path
     else
-
+      flash[:notice] = "Please fill in all the boxes"
       render :new
     end
   end

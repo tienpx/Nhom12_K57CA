5e1eea44dab227d9ef79ea48e2cfe10fe9ea83cb
trinhthithu <trinhthithu94@gmail.com>
2014-04-17 23:58:34 +0700

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:signin][:password])
       session[:@user_id] = @user.id
     else
-      flash[:error] = "Your account haven't yet been registered. Please Singing up first!"
+      flash[:error] = "Your account haven't been registered yet. Please Singing up first!"
       render :new
     end
   end

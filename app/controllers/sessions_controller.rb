346f8fd9492e2c6a1a2e81495783b7243f822953
thuynt_57 <ngthuy1511@gmail.com>
2014-04-24 09:30:29 -0700

    Redirect to the home page after sign in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.where(:name => params[:signin][:name]).first
     if @user && @user.authenticate(params[:signin][:password])
       session[:@user_id] = @user.id
+      redirect_to user_path(@user)
     else
       flash[:error] = "Your account haven't been registered yet. Please Singing up first!"
       render :new

150ba99a61c2b68aa0d996e6b0484943f6e776a5
thuynt-57 <ngthuy1511@gmail.com>
2014-05-05 15:10:22 -0700

    Rewrite test for user management.

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 feature 'Signing up' do
+  scenario 'Sign up title' do
+    visit '/'
+    click_link 'Sign up'
+    expect(page).to have_title(" | Sign up")
+  end
+
   scenario 'Successful sign up' do
     visit '/'
     click_link 'Sign up'
@@ -11,17 +17,49 @@ feature 'Signing up' do
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
-    expect(page).to have_content("You have signed up successfully Let start and enjoy yourself")
+    expect(page).to have_content("You have signed up successfully")
   end
   
   scenario 'sign up with password comfirmation not match' do
     visit '/'
-	  click_link 'Sign up'
+    click_link 'Sign up'
     fill_in "Username", with: "thuynt"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "anything"
     click_button "Sign up"
     expect(page).to have_content("Password confirmation doesn't match Password")
+  end
+
+  scenario "email format is not invalid" do
+    visit '/'
+    click_link 'Sign up'
+    fill_in "Username", with: "thuynt"
+    fill_in "Email", with: "user@example"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    
+    expect(page).to have_content("Email is invalid")
+  end
+
+  scenario "email address is already taken" do
+    visit '/'
+    click_link 'Sign up'
+    fill_in "Username", with: "Alice"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    visit '/'
+    click_link 'Sign up'
+    fill_in "Username", with: "thuynt"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("Email has already been taken")
   end
 end

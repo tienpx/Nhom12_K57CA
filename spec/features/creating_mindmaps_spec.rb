36b69620a536488e1bd9380de27c515375d94a20
TrangVu <trangvt_57@vnu.edu.vn>
2014-05-06 23:36:12 +0700

    feature create new mindmap belongs to user

---
+++ spec/features/creating_mindmaps_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature "Creating Mindmaps" do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    visit '/'
+    click_link "Sign in"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    click_link "New"
+  end
+
+  scenario "Creating a mindmap" do
+    fill_in "Title", with: "bbb"
+    click_button "Create Mindmap"
+    
+    expect(page).to have_content("Mindmap has been created.")
+  end
+
+  scenario "Creating a mindmap without valid attributes fails" do
+    click_button "Create Mindmap"
+    expect(page).to have_content("Title can't be blank")
+  end
+end

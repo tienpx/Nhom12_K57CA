b0e6971af717bd0e43938c83f3e6c187a289af52
TrangVu <trangvt_57@vnu.edu.vn>
2014-05-06 23:47:15 +0700

    all features for mindmap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313105037) do
+ActiveRecord::Schema.define(version: 20140506142552) do
+
+  create_table "mindmaps", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "mindmaps", ["user_id"], name: "index_mindmaps_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
